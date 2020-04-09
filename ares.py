842abd11b5d21fa57f4d0eb7c96e9ae069c8e3c5
DanielSCrouch <d_crouch@outlook.com>
2020-04-09 13:05:41 -0700

    ares executable

--- ares.py
+++ ares.py
@@ -1,4 +1,6 @@
-# Main Ares module to be called on start-up. Alias for console.py 
+#!/usr/bin/env python3
+
+# Main Ares module to be called on start-up. Alias for console.py
 #
 # Author: Daniel Crouch
 # Date created: March 2020
