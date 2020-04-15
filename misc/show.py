5ce35c6a159b3a6285b4403bff443106977961f2
DanielSCrouch <d_crouch@outlook.com>
2020-04-15 06:49:56 -0700

    demo

---
+++ misc/show.py
@@ -0,0 +1,12 @@
+>>> plan t2
+[*] planner resolved
+
+    Attack Vector
+    ============================================================
+    step    0: EXPLOIT_MSQL_BRUTE_FORCE T1
+            1: EXPLOIT_TOKENS T1
+            2: EXPLOIT_HASHDUMP T1
+            3: EXPLOIT_PSEXEC T1 T2
+            4: EXPLOIT_PERSISTENCE T2
+
+>>>
